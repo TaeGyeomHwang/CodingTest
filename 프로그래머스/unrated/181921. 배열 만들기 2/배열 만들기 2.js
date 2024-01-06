6ec2cb1b3cef10ad1643b84566df0cee3997620f
DriedPollack <jihuhw99@gmail.com>
2024-01-06 16:43:56 +0900

    [level unrated] Title: 배열 만들기 2, Time: 19.02 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,13 +1,13 @@
 function solution(l, r) {
-    var answer = [];
-    //  주어진 범위 내에서 반복
-    for(let i=l;i<=r;i++){
-        //  현재 숫자를 문자열로 변환해서 저장
-        let arr=String(i);
-        //  문자열로 변환된 숫자의 각 자릿수를 확인. 5 또는 0 이외의 숫자라면 건너뛴다.
-        if(![...arr].every(x=>x==='5'||x==='0'))continue;
-        //  조건을 만족하면 push
-        answer.push(i)
+    const result = [];
+    
+    // 반복문의 범위 설정
+    for (let i = l; i <= r; i++) {
+        //  문자열로 바꾼 뒤 각 자릿수를 확인, 5와 0만 있을 경우 push
+        if ([...String(i)].every(num => num === "0" || num === "5")) {
+            result.push(i);
+        }
     }
-    return answer.length? answer:[-1];
+    //  아무 숫자도 없다면 -1 반환
+    return result.length > 0 ? result : [-1];
 }
