e844b265244c0d0babdebf8269e1061d82af9373
DriedPollack <jihuhw99@gmail.com>
2024-01-09 08:52:18 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -2,17 +2,14 @@ function solution(my_string, is_prefix) {
     let arr = [];
     for(let i=0; i<my_string.length; i++){
         if(i==0){
-            arr.push(my_string);
+            if(my_string==is_prefix){
+                return 1;
+            }
         }else{
-            arr.push(my_string.slice(0,i));
+            if(my_string.slice(0,i)==is_prefix){
+                return 1;
+            }
         }
     }
-    
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]==is_prefix){
-            return 1;
-        }
-    }
-    
     return 0;
 }
