13b2e1cf71682256cd199546f4dd0ef8dcbbfdeb
DriedPollack <jihuhw99@gmail.com>
2024-01-12 14:26:30 +0900

    [level Lv.0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/Lv.0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -8,8 +8,18 @@ class Solution {
         arr1 = Arrays.copyOfRange(num_list, n, num_list.length);
         arr2 = Arrays.copyOfRange(num_list, 0, n);
         
-        int[] arr3 = Arrays.copyOfRange(arr1, 0, arr1.length + arr2.length);
-        System.arraycopy(arr2, 0, arr3, arr1.length, arr2.length);
+        // int[] arr3 = Arrays.copyOfRange(arr1, 0, arr1.length + arr2.length);
+        // System.arraycopy(arr2, 0, arr3, arr1.length, arr2.length);
+        
+        int[] arr3 = new int[arr1.length+arr2.length];
+        for(int i=0, j=0; i<arr3.length; i++){
+            if(i<arr1.length){
+                arr3[i]=arr1[i];
+            }else{
+                arr3[i]=arr2[j];
+                j++;
+            }
+        }
         
         return arr3;
     }
