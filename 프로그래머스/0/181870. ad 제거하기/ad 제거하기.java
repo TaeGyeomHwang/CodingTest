a5f733c62b0f5b3c9d84b74ae92f6d81bfa0fce1
DriedPollack <jihuhw99@gmail.com>
2024-01-16 22:25:54 +0900

    [level 0] Title: ad 제거하기, Time: 2.49 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -2,11 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] strArr) {
-        
-        strArr = Arrays.stream(strArr)
-                .filter(item -> !item.contains("ad"))
-                .toArray(String[]::new);
-        
-        return strArr;
+        return Arrays.stream(strArr).filter(item -> !item.contains("ad")).toArray(String[]::new);
     }
 }
