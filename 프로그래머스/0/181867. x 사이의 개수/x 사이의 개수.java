f7cc74ac3ec2c9ccd05244032c7aa66218bbecb5
DriedPollack <jihuhw99@gmail.com>
2024-01-17 10:20:04 +0900

    [level 0] Title: x 사이의 개수, Time: 12.66 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(String myString) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        int length = 0;
+        
+        for(int i=0; i<=myString.length(); i++){
+            if(i==myString.length()){
+                answer.add(length);
+                break;
+            }
+            if(myString.charAt(i)!='x'){
+                length++;
+            }else{
+                answer.add(length);
+                length = 0;
+            }
+        }
+        
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
