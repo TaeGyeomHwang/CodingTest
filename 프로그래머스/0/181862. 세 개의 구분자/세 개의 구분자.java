825d3bd800a1365155eb0f194a28704630674bcb
DriedPollack <jihuhw99@gmail.com>
2024-01-18 10:01:27 +0900

    [level 0] Title: 세 개의 구분자, Time: 81.98 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String myStr) {
+        String[] result;
+        
+        ArrayList<String> list = new ArrayList<>();
+        String str = "";
+        
+        for(int i=0; i<=myStr.length(); i++){
+            if(i == myStr.length()){
+                list.add(str);
+                break;
+            }
+            if(myStr.charAt(i)!='a' && myStr.charAt(i)!='b' && myStr.charAt(i)!='c'){
+                str += myStr.charAt(i);
+            }else{
+                list.add(str);
+                str = "";
+            }
+        }
+        result = list.stream().filter(item -> !item.equals("")).toArray(String[]::new);
+
+        if(result.length==0){
+            result = new String[] {"EMPTY"};
+            return result;
+        }else{
+            return result;
+        }
+    }
+}
