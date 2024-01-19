944b2aa9e53ebcf1559cd71b6d7681b5379206a4
DriedPollack <jihuhw99@gmail.com>
2024-01-19 22:39:49 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 2.79 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        // Set<Integer> set = new TreeSet<>();
-        // for(int i : num_list){
-        //     set.add(i);
-        // }
-        //처음부터 5개만 넣는법?
-        // List<Integer> list = new ArrayList<> (set);
-        // for(int i=list.size()-1; i>4; i--){
-        //     list.remove(i);
-        // }
         Arrays.sort(num_list);
         List<Integer> list = new ArrayList<> ();
         for(int i: num_list){
@@ -19,6 +10,8 @@ class Solution {
             }
             list.add(i);
         }
+
         return list.stream().mapToInt(e -> e).toArray();
     }
 }
+
