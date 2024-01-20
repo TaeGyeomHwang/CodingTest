247d367a9c925b1af14a240fe53219c67f61d8d4
DriedPollack <jihuhw99@gmail.com>
2024-01-20 11:44:34 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.34 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<rank.length; i++){
+            if(attendance[i]){
+                map.put(rank[i], i);
+            }
+        }
+        
+        List<Integer> number = new ArrayList<>(map.keySet());
+        Collections.sort(number);
+        
+        return 10000*map.get(number.get(0)) + 100*map.get(number.get(1)) + map.get(number.get(2));
+    }
+}
