eab0d4179b2fc8f28cb928453e0aa2a62804f64d
DriedPollack <jihuhw99@gmail.com>
2024-01-21 11:25:55 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,12 +1,17 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        if(date1[0]<date2[0]){
-            return 1;
-        }else if(date1[0]==date2[0] && date1[1]<date2[1]){
-            return 1;
-        }else if(date1[0]==date2[0] && date1[1]==date2[1] && date1[2]<date2[2]){
-            return 1;
-        }
-        return 0;
+        // if(date1[0]<date2[0]){
+        //     return 1;
+        // }else if(date1[0]==date2[0] && date1[1]<date2[1]){
+        //     return 1;
+        // }else if(date1[0]==date2[0] && date1[1]==date2[1] && date1[2]<date2[2]){
+        //     return 1;
+        // }
+        // return 0;
+        
+        int intDate1 = date1[0] * 10000 + date1[1] * 100 + date1[2];
+        int intDate2 = date2[0] * 10000 + date2[1] * 100 + date2[2];
+
+        return (intDate1 < intDate2) ? 1 : 0;
     }
 }
