ce2c5bd483165c97b05a2a8f7d31046c7f724705
DriedPollack <jihuhw99@gmail.com>
2024-01-25 17:14:50 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,11 +1,9 @@
-class Solution {
+class Solution {  
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        int max = 0;
         for(int i=0; i<array.length; i++){
-            if(max<array[i]){
-                max = array[i];
-                answer[0] = max;
+            if(array[i]>answer[0]){
+                answer[0] = array[i];
                 answer[1] = i;
             }
         }
