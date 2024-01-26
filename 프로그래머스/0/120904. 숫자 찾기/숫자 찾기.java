c9606048b0f09dbb503bff0e931048a9fd8f21be
DriedPollack <jihuhw99@gmail.com>
2024-01-26 23:47:24 +0900

    [level 0] Title: 숫자 찾기, Time: 5.67 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int num, int k) {
-        // String[] numArr = (""+num).split("");
-        // String kStr = ""+k;
-        // for(int i=0; i<numArr.length; i++){
-        //     if(numArr[i].equals(kStr)){
-        //         return i+1;
-        //     }
-        // } 
-        // return -1;
-        return (" "+num).indexOf(String.valueOf(k));
+        String[] numArr = (""+num).split("");
+        String kStr = ""+k;
+        for(int i=0; i<numArr.length; i++){
+            if(numArr[i].equals(kStr)){
+                return i+1;
+            }
+        }  
+        return -1;
     }
 }
