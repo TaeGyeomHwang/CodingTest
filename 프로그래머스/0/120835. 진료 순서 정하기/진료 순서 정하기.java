2301a28e4895f1f24244d2fc8898fab3f688eaa2
DriedPollack <jihuhw99@gmail.com>
2024-01-26 23:06:17 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,15 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] emergency) {
+        //         풀이 1
+        // int[] answer = new int[emergency.length];
+        // Integer[] arr = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
+        // Arrays.sort(arr, Collections.reverseOrder());
+        // for(int i=0; i<emergency.length; i++){
+        //     for(int j=0; j<arr.length; j++){
+        //         if(arr[j]==emergency[i]){
+        //             answer[i] = j+1;
+        //         }
+        //     }
+        // }
+//          풀이 2
         int[] answer = new int[emergency.length];
-        Integer[] arr = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr, Collections.reverseOrder());
         for(int i=0; i<emergency.length; i++){
-            for(int j=0; j<arr.length; j++){
-                if(arr[j]==emergency[i]){
-                    answer[i] = j+1;
+            int idx = 1;
+            for(int j=0; j<emergency.length; j++){
+                if(emergency[i]<emergency[j]){
+                    idx++;
                 }
             }
+            answer[i] = idx;
         }
         return answer;
     }
