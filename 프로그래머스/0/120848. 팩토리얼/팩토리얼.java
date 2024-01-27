43177c50665d3de586c86002aae4a4d3030212c2
DriedPollack <jihuhw99@gmail.com>
2024-01-27 21:22:51 +0900

    [level 0] Title: 팩토리얼, Time: 0.21 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int ans = 1;
+        while(ans<=n){
+            System.out.println(ans);
+            ans *= i;
+            i++;
+        }
+        return i-2;
+    }
+}
