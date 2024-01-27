548626796aad6d6ceb94ea363d426bbfa7c04750
DriedPollack <jihuhw99@gmail.com>
2024-01-27 21:44:15 +0900

    [level 0] Title: k의 개수, Time: 85.04 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
+        String num = ""+k;
         for(int a=i; a<=j; a++){
             String[] arr = (""+a).split("");
             for(int b=0; b<arr.length; b++){
-                if(arr[b].equals(""+k)){
+                if(arr[b].equals(num)){
                     answer++;
                 }
             }
