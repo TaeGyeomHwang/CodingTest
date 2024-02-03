0fe78b9bd12a9b42d77437e4fb8b2551056f4fdb
DriedPollack <jihuhw99@gmail.com>
2024-02-03 20:30:45 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,20 +1,27 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[][] dots) {
-        Arrays.sort(dots, (a, b) -> {
-            return a[0] - b[0];
-        });
-
-        int x1 = dots[0][0], y1 = dots[0][1];
-        int x2 = dots[1][0], y2 = dots[1][1];
-        int x3 = dots[2][0], y3 = dots[2][1];
-        int x4 = dots[3][0], y4 = dots[3][1];
-
-        if ((y2 - y1) * (x4 - x3) == (y4 - y3) * (x2 - x1)) {
-            return 1;
-        } else {
-            return 0;
-        }
+        int x1 = dots[0][0];
+        int y1 = dots[0][1];
+        int x2 = dots[1][0];
+        int y2 = dots[1][1];
+        int x3 = dots[2][0];
+        int y3 = dots[2][1];
+        int x4 = dots[3][0];
+        int y4 = dots[3][1];
+        int answer = 0;
+        
+        double slope1 = (double) (y2 - y1) / (x2 - x1);
+        double slope2 = (double) (y4 - y3) / (x4 - x3);
+        if (slope1 == slope2) answer = 1;
+        
+        slope1 = (double) (y3 - y1) / (x3 - x1);
+        slope2 = (double) (y2 - y4) / (x2 - x4);
+        if (slope1 == slope2) answer = 1;
+        
+        slope1 = (double) (y4 - y1) / (x4 - x1);
+        slope2 = (double) (y2 - y3) / (x2 - x3);
+        if (slope1 == slope2) answer = 1;
+        
+        return answer;
     }
 }
