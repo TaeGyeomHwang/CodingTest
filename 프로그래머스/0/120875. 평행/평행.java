b12ac0f113e498c1de6647221a6502fda4785aba
DriedPollack <jihuhw99@gmail.com>
2024-02-03 20:31:39 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int[][] dots) {
-        int x1 = dots[0][0];
-        int y1 = dots[0][1];
-        int x2 = dots[1][0];
-        int y2 = dots[1][1];
-        int x3 = dots[2][0];
-        int y3 = dots[2][1];
-        int x4 = dots[3][0];
-        int y4 = dots[3][1];
+        int x1 = dots[0][0], y1 = dots[0][1];
+        int x2 = dots[1][0], y2 = dots[1][1];
+        int x3 = dots[2][0], y3 = dots[2][1];
+        int x4 = dots[3][0], y4 = dots[3][1];
         int answer = 0;
         
         double slope1 = (double) (y2 - y1) / (x2 - x1);
