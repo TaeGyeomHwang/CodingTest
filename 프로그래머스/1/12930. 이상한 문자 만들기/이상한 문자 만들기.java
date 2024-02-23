b5052bb93a105c232030dfa4f3f40a4950ca3c60
DriedPollack <jihuhw99@gmail.com>
2024-02-23 21:40:40 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb  = new StringBuilder();
+        int index = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==' '){
+                index=0;
+                sb.append(' ');
+            }
+            else{
+                if(index%2==0) sb.append(Character.toUpperCase(s.charAt(i)));
+                else sb.append(Character.toLowerCase(s.charAt(i)));
+                index++;
+            }
+        }
+        return sb.toString();
+    }
+}
