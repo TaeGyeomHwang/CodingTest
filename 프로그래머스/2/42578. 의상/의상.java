6fe4a0a2aa309a87652e85542830f866b989803f
DriedPollack <jihuhw99@gmail.com>
2024-03-15 20:43:20 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> clothesMap = new HashMap<>();
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            clothesMap.put(type, clothesMap.getOrDefault(type, 0) + 1);
+        }
+        int answer = 1;
+        for (int count : clothesMap.values()) {
+            answer *= (count + 1); 
+        }
+        return answer - 1;
+    }
+}
