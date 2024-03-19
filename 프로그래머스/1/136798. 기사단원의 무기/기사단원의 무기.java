2c529833b20390f56bde44e106fbc9a7c2134d0c
DriedPollack <jihuhw99@gmail.com>
2024-03-19 09:47:02 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.43 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,23 +1,16 @@
-import java.util.*;
 class Solution {
     public int solution(int number, int limit, int power) {
         int result = 0;
         int[] arr = new int[number];
         for(int i=1; i<=number; i++){
-            // for(int j=1; j<=i; j++){
-            //     if(i%j==0)
-            //         arr[i-1]++;
-            // }
-            List<Integer> divisorList = new ArrayList<>();
             for (int j = 1; j * j <= i; j++) {
                 if (i % j == 0) {
-                    divisorList.add(j);
+                    arr[i-1]++;
                     if (j != i / j) {
-                        divisorList.add(i / j);
+                        arr[i-1]++;
                     }
                 }
             }
-            arr[i-1] = divisorList.size();
         }
         for(int i=0; i<arr.length; i++){
             if(arr[i]>limit)
